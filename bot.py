5f4b4ad5377b729fe054b4fc9c0acbc8ed46f0bf
tqt001 <53989908+tqt001@users.noreply.github.com>
2020-06-12 18:12:03 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,33 +1,41 @@
+import random
+import os
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix='.')
+TOKEN = "NzIxMDY5NjAxMjIzNjA2Mzkz.XuQFZw.JfEsOFthbPgispobyOFiA7Jwuok"
+
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('with your girlfriend ;)'))
     print('Bot is ready.')
 
-@client.event
-async def on_member_join(member):
-    print(f'Oh god, look at {member}.')
 
 @client.event
-async def on_member_remove(member):
-    print(f'Everyone say bye {member}.')
+async def on_member_join(ctx, member):
+    await ctx.send(f'Oh god, look at {member}.')
+
+
+@client.event
+async def on_member_remove(ctx, member):
+    await ctx.send(f'Everyone say bye {member}.')
+
 
 @client.command()
-async def 
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
 
+@client.command()
+async def clear(ctx, amount=50):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f'Clear complete.')
 
 
+for file in os.listdir('./cogs'):
+    if file.endswith('.py'):
+        client.load_extension(f'cogs.{file[:-3]}')
 
-
-
-
-
-
-
-
-
-client.run("NzIxMDY5NjAxMjIzNjA2Mzkz.XuP0Mg.PS9J2DIJmXau8nczJwbY4QYU8sA")
+client.run(TOKEN)
